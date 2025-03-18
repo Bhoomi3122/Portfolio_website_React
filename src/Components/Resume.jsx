3b19f23adde9a3c15baaff2400e08194dc35f54a
aGRAWAL <gargbhoomi01@gmail.com>
2025-03-19 00:33:30 +0530

    removed download option

--- src/Components/Resume.jsx
+++ src/Components/Resume.jsx
@@ -3,7 +3,7 @@ import { FiExternalLink, FiDownload } from "react-icons/fi";
 import '../Styles/Resume.css';
 const Resume = () => {
   const handleDownload = async () => {
-    const response = await fetch('/Bhoomi_Garg_Resume_File.pdf');  // Fetch file
+    const response = await fetch('/Bhoomi_Garg_Resume_File.pdf', { cache: 'no-store' });  // Fetch file
     const blob = await response.blob();  // Convert to binary
     const link = document.createElement('a');
     link.href = URL.createObjectURL(blob);
@@ -11,6 +11,7 @@ const Resume = () => {
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
+    URL.revokeObjectURL(link.href);
   };
   return (
     <div id="outer-div">
@@ -18,9 +19,9 @@ const Resume = () => {
       <div id="online-div">
       <a href="https://drive.google.com/file/d/1kdasF2EMpWTzMTf5AG04L0peifENVxLN/view?usp=sharing" target='_blank' rel='noopener noreferrer'><FiExternalLink className='icon'/>View Resume Online</a>
       </div>
-      <div id="download-div">
+      {/* <div id="download-div">
       <button onClick={handleDownload}><FiDownload className='icon'/>Download Resume</button>
-      </div>
+      </div> */}
     </div>
   )
 }
