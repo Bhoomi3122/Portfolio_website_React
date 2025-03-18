beeba667c6a9bb3952fdb944db224c960a70a53d
aGRAWAL <gargbhoomi01@gmail.com>
2025-03-19 00:26:55 +0530

    Updated Resume

--- src/Components/Resume.jsx
+++ src/Components/Resume.jsx
@@ -3,7 +3,7 @@ import { FiExternalLink, FiDownload } from "react-icons/fi";
 import '../Styles/Resume.css';
 const Resume = () => {
   const handleDownload = async () => {
-    const response = await fetch('/Resume_Bhoomi.pdf');  // Fetch file
+    const response = await fetch('/Bhoomi_Garg_Resume_File.pdf');  // Fetch file
     const blob = await response.blob();  // Convert to binary
     const link = document.createElement('a');
     link.href = URL.createObjectURL(blob);
@@ -16,7 +16,7 @@ const Resume = () => {
     <div id="outer-div">
       <h2>Resume</h2>
       <div id="online-div">
-      <a href="https://drive.google.com/file/d/1dgpbINwDYerNSLDN_Bs5OHebP892K5ep/view?usp=sharing" target='_blank' rel='noopener noreferrer'><FiExternalLink className='icon'/>View Resume Online</a>
+      <a href="https://drive.google.com/file/d/1kdasF2EMpWTzMTf5AG04L0peifENVxLN/view?usp=sharing" target='_blank' rel='noopener noreferrer'><FiExternalLink className='icon'/>View Resume Online</a>
       </div>
       <div id="download-div">
       <button onClick={handleDownload}><FiDownload className='icon'/>Download Resume</button>
